886305cec8e7b4c904f7a67b62961e1babf66902
Náder W. S. Sírio <nadersirio@gmail.com>
2024-01-29 20:05:01 -0300

    add: content-component finalized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
-import { BannerTitleComponent } from './components/banner-title/banner-title.component';
+import { BannerMainComponent } from './components/banner-main/banner-main.component';
 import { BannerContentComponent } from './components/banner-content/banner-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
-    BannerTitleComponent,
+    BannerMainComponent,
     BannerContentComponent
   ],
   imports: [
---
+++ src/app/components/banner-main/banner-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-main',
+  templateUrl: './banner-main.component.html',
+  styleUrls: ['./banner-main.component.css']
+})
+export class BannerMainComponent {
+
+}
