15b74b26f5e2db77030ffd5f75751eb13cb5c2c6
Náder W. S. Sírio <nadersirio@gmail.com>
2024-02-01 15:28:08 -0300

    fix: organizing files and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
-import { BannerMainComponent } from './components/banner-main/banner-main.component';
-import { BannerContentComponent } from './components/banner-content/banner-content.component';
+import { ContentMainComponent } from './components/content-main/content-main.component';
+import { FieldMainContentComponent } from './components/banner-content/field-main-content.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
-    BannerMainComponent,
-    BannerContentComponent
+    ContentMainComponent,
+    FieldMainContentComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/banner-content/field-main-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-field-main-content',
+  templateUrl: './field-main-content.component.html',
+  styleUrls: ['./field-main-content.component.css']
+})
+export class FieldMainContentComponent {
+
+}
